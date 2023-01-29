7b656ce9f469060c04f58bfa4df0d051bedb29e2
Sam O'Cain <samocain93@gmail.com>
2023-01-29 17:41:09 -0600

    week 4 done

--- algorithms.js
+++ algorithms.js
@@ -72,5 +72,18 @@ var countdown = function(num) {
 
 // WEEK 4 || #3 - 
 
+// Write code to add all the numbers in `arr` and return the total
+
+var sumArray = function(arr) {
+    let sum = 0;
+
+    for (let i = 0; i < arr.length; i++) {
+        let currentNumber = arr[i];
+        sum += currentNumber;
+
+    }
+    return sum;
+};
+
 
 
