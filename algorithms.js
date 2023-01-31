3d9ee37d318f40b5b0004d7e5ddd0a826a09ef39
Sam O'Cain <samocain93@gmail.com>
2023-01-31 00:56:07 -0600

    solved fizzbuzz

--- algorithms.js
+++ algorithms.js
@@ -86,4 +86,35 @@ var sumArray = function(arr) {
 };
 
 
+// WEEK 5 || # 1 - FIZZ BUZZ //
+
+// Write code to loop through the array of numbers
+// At each iteration, if a number is evenly divisible by 3 print "Fizz"
+// If a number is evenly divisible by 5 print "Buzz"
+// If a number is evenly divisible by both 3 AND 5, print "Fizz Buzz"
+// If a number is not divisible by 3 or 5, print the number
+
+
+
+function fizzBuzz(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        let index = arr[i];
+
+        if (index % 3 === 0 && index % 5 === 0){
+            console.log("Fizz Buzz")
+        } else if (index % 3 === 0){
+            console.log("Fizz")
+        } else if (index % 5 === 0){
+            console.log("Buzz")
+        } else {
+            console.log(index)
+        }
+    }
+    
+}
+
+
+
+
+
 
